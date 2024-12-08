bd4ebdf215177235cc68c009092a6e9a2c17815b
jcgt26 <juancamiloguzmant36@gmail.com>
2024-12-08 22:43:50 +0100

    Added more pages and layout, also added fonts on root-layout.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Inter, Lusitana} from "next/font/google";
+
+export const inter = Inter({subsets: ['latin']}) // in case want to import extra char like cyrillic has to happen here
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400']})
